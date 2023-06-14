bfed929cb2b8f3ddc828c2b9eba18837c407d616
Dylan <dylan.lumsden@studytafensw.edu.au>
2023-06-14 17:58:48 +1000

    Fixed Photos showing on home.js

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyBRLU8My3cFx7Q9j05RlpGcADE2jSpHKvs",
-    authDomain: "classproject-937e8.firebaseapp.com",
-    projectId: "classproject-937e8",
-    storageBucket: "classproject-937e8.appspot.com",
-    messagingSenderId: "669600973558",
-    appId: "1:669600973558:web:72e86eca25137e80d622d7"
+    apiKey: "AIzaSyDu-SSnxfKczn9euc9C0qmw2GDsiZIYSSM",
+  authDomain: "movie-assessment-2b308.firebaseapp.com",
+  projectId: "movie-assessment-2b308",
+  storageBucket: "movie-assessment-2b308.appspot.com",
+  messagingSenderId: "160340271656",
+  appId: "1:160340271656:web:93023e5d57ca148fb7cb1d"
 };
   
