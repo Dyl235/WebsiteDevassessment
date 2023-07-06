f8f8870e3ed52daa2237e513922dcbc914ae0ede
Dylan <dylan.lumsden@studytafensw.edu.au>
2023-07-06 20:50:10 +1000

    Updated About.js and Contact.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,18 @@
 import '../styles/about.css'
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col'
+
+
 export function About() {
     return (
-        <><h1 class="tests">About</h1>
-        <h2 class="tests2">Welcome to My Movie Club!</h2></>
-
+        <Container>
+        <Col md="10">
+        <div>
+            <h2>Under Construction</h2>
+            <center><h1>About Dylan's Film Reviews</h1></center>
+            <h4>Welcome to  My Film Review Website here you will find the Movies that I enjoy watching.. More to Come Soon!</h4>
+        </div>
+        </Col>
+    </Container>
     )
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,6 +2,8 @@ export function Contact () {
     return (
         <div>
            <h1><center>Contact</center></h1>
+           <h4><center>If things are not working properly contact me so I can fix them!
+            Thanks!</center></h4>
         </div>
     )
 }
